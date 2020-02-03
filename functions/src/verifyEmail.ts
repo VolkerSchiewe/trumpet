4c056d022a42a4b32027c5c37b7f4c081640aab3
VolkerSchiewe <volker.s1994@gmail.com>
2020-02-03 20:21:58 +0100

    add tests for verifyEmail function and test correct failure of registration

--- functions/src/verifyEmail.ts
+++ functions/src/verifyEmail.ts
@@ -5,20 +5,23 @@ import {DB} from "./utils/constants";
 
 export default (admin: admin.app.App) => async (req: Request, res: Response) => {
   console.log(req.query.token);
-  const document = await admin.firestore().collection(DB.PARTICIPANTS_COLLECTION).doc(req.query.token);
-
-  const data = (await document.get()).data();
-  const created = data ? new Date(data[DB.CREATED]) : null;
-  const ONE_HOUR = 60 * 60 * 1000; /* ms */
-  const now = new Date();
-  if (created && ((now.getTime()) - created.getTime()) > ONE_HOUR){
-    console.log("More than an hour ago");
-    res.status(400);
-    res.send("Verification code is expired!");
-    return
+  try {
+    const document = await admin.firestore().collection(DB.PARTICIPANTS_COLLECTION).doc(req.query.token);
+    const data = (await document.get()).data();
+    const created = data ? new Date(data[DB.CREATED]) : null;
+    const ONE_HOUR = 60 * 60 * 1000; /* ms */
+    const now = new Date();
+    if (created && ((now.getTime()) - created.getTime()) > ONE_HOUR) {
+      console.log("More than an hour ago");
+      res.status(400);
+      res.send("Verification code is expired!");
+      return
+    }
+    console.log("Verification completed");
+    await document.update({[DB.EMAIL_VERIFIED]: now.toISOString()});
+    res.status(200).send("Verification completed")
+  } catch (e) {
+    console.log("Document not found");
+    res.status(400).send("Wrong Link!")
   }
-  console.log("Verification completed");
-  await document.update({[DB.EMAIL_VERIFIED]: now.toISOString()});
-  res.status(200);
-  res.send("Verification completed")
 }
--- functions/test/registration.test.ts
+++ functions/test/registration.test.ts
@@ -3,20 +3,33 @@ import {expect} from 'chai';
 import * as sinon from 'sinon';
 import * as validateRecaptcha from "../src/helper/validateRecaptcha"
 import * as sendMail from "../src/helper/sendMail"
+import {SinonStub} from "sinon";
 
 require('firebase-functions-test')({
   projectId: 'trumpet-dev-7521f',
 }, '../keys/devKeys.json');
 
-// TODO mock config
 
-const cloudFunctions = require('../src/index'); // relative path to functions code
+const cloudFunctions = require('../src/index');
 
-describe("Cloud functions", () => {
-  it('should test register function', (done) => {
-    const recaptcha = sinon.stub(validateRecaptcha, "validateRecaptcha");
-    sinon.stub(sendMail, "sendMail");
-    const req = {body: {name: "name", email: "email1", recaptchaToken: "token"}, protocol: "http", hostname: "example"};
+describe("Registration cloud function", () => {
+  let recaptcha: SinonStub<any, any>;
+  let mail: SinonStub<any, any>;
+  before(() => {
+    recaptcha = sinon.stub(validateRecaptcha, "validateRecaptcha");
+    mail = sinon.stub(sendMail, "sendMail");
+  });
+  afterEach(() => {
+    recaptcha.resetBehavior();
+    mail.resetBehavior();
+  });
+
+  it('should test registration success', (done) => {
+    const req = {
+      body: {name: "one", email: "one@email.com", recaptchaToken: "token"},
+      protocol: "http",
+      hostname: "example"
+    };
     const res = {
       send: (message: string) => {
       },
@@ -28,5 +41,33 @@ describe("Cloud functions", () => {
       }
     };
     cloudFunctions.checkRegistration(req, res)
+  });
+
+  it('should fail if registration is called twice with same mail', (done) => {
+    const req = {
+      body: {name: "two", email: "two@email.com", recaptchaToken: "token"},
+      protocol: "http",
+      hostname: "example"
+    };
+    const resFirst = {
+      send: (message: string) => {
+      },
+      status: function (status: number) {
+        return this
+      }
+    };
+    const res = {
+      send: (message: string) => {
+      },
+      status: function (status: number) {
+        expect(status).to.eq(400);
+        done();
+        return this
+      }
+    };
+    cloudFunctions.checkRegistration(req, resFirst);
+    setTimeout(() => {
+      cloudFunctions.checkRegistration(req, res)
+    }, 1000)
   });
 });
