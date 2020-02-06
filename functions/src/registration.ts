a3f652860263a1ee8a19de5861d8335303da9791
VolkerSchiewe <volker.s1994@gmail.com>
2020-02-06 14:39:41 +0100

    adjust registration function to store all data in post

--- functions/src/registration.ts
+++ functions/src/registration.ts
@@ -7,24 +7,18 @@ import * as admin from "firebase-admin";
 import {DB} from "./utils/constants";
 
 export default (admin: admin.app.App) => async (req: Request, res: Response) => {
-  const recaptchaToken = req.body.recaptchaToken;
-  const name = req.body.name;
-  const email = req.body.email;
-  console.log(`Called with: ${name}, ${email}, ${recaptchaToken}`);
+  const {recaptchaToken, ...data} = req.body;
+  console.log(`Registering: ${data.firstName} ${data.lastName}, ${data.email}`);
   try {
     await validateRecaptcha(recaptchaToken); // throws if something is wrong
-    const registeredMails = await admin.firestore().collection(DB.PARTICIPANTS_COLLECTION).where(DB.EMAIL, "==", email).get();
+    const registeredMails = await admin.firestore().collection(DB.PARTICIPANTS_COLLECTION).where(DB.EMAIL, "==", data.email).get();
     if (registeredMails.size > 0)
       throw new Error("Email is already registered!");
-    const data = {
-      [DB.CREATED]: new Date().toISOString(),
-      [DB.NAME]: name,
-      [DB.EMAIL]: email,
-    };
+
     const doc = await admin.firestore().collection(DB.PARTICIPANTS_COLLECTION).add(data);
     console.log(`Stored document ${doc.id} with data: ${JSON.stringify(data)}`);
 
-    const mail = new RegistrationCompleteMail(name, email, `${req.protocol}://${req.hostname}/verifyMail?token=${doc.id}`);
+    const mail = new RegistrationCompleteMail(data.firstName, data.email, `${req.protocol}://${req.hostname}/verifyMail?token=${doc.id}`);
     await sendMail(mail);
     res.status(200).send();
   } catch (e) {
