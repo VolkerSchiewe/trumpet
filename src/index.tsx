e0849af64cd770be50df10e3658163a74eeff43c
VolkerSchiewe <volker.s1994@gmail.com>
2020-03-04 18:05:33 +0100

    add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import * as ReactDOM from "react-dom";
 import {registerServiceWorker} from "./utils/registerServiceWorker";
 import App from "./components/App";
 import i18n from "./i18n/i18n";
+import * as Sentry from "@sentry/browser"
+
+Sentry.init({dsn: "https://c8dd35ce4f6b477aa520e6ea7ebc4574@sentry.io/3651952"});
 
 if (process.env.NODE_ENV == "production")
   registerServiceWorker();
 
-i18n()
+i18n();
 
 ReactDOM.render(
   <App/>,
