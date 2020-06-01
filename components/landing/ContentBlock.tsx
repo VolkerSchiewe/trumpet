300d7c90a9ee4dcffbd3dc3b9358b0881e7d8810
VolkerSchiewe <volker.s1994@gmail.com>
2020-06-01 20:13:20 +0200

    Faster hover animation

--- components/landing/ContentBlock.tsx
+++ components/landing/ContentBlock.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const ContentBlock: React.FC<Props> = ({color, title, content}) => {
     return (
-        <div className={`border-${color} text-${color} hover:text-white hover:bg-${color} border p-5 transform hover:scale-105 transition-all duration-500 ease-in-out hover:shadow-2xl`}>
+        <div className={`border-${color} text-${color} hover:text-white hover:bg-${color} border p-5 transform hover:scale-105 transition-all duration-300 ease-in-out hover:shadow-2xl`}>
             <h2 className={`font-medium italic text-lg mb-2`}>{title}</h2>
             <Divider/>
             <p className={`font-light text-sm font-sans-content my-2`}>
