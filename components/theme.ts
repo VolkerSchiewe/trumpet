270fee3ed8893c742dbbed2316cca512767ce9cb
VolkerSchiewe <volker.s1994@gmail.com>
2020-06-13 17:50:51 +0200

    Remove colors from MUI theme

--- components/theme.ts
+++ components/theme.ts
@@ -4,12 +4,6 @@ const theme = responsiveFontSizes(createMuiTheme({
     typography:{
         fontFamily: "'Fira Sans', Arial, sans-serif"
     },
-    palette:{
-        blue: "#0097dc",
-        yellow: "#f19938",
-        green: "#00c835",
-        magenta: "#f00079",
-    }
 }));
 
 export default theme;
