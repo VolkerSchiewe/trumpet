0174f245179131b53ac164657315038eba7de2ca
VolkerSchiewe <volker.s1994@gmail.com>
2020-06-14 12:29:15 +0200

    Increase text size in ContentBlock

--- components/landing/ContentBlock.tsx
+++ components/landing/ContentBlock.tsx
@@ -15,9 +15,9 @@ const ContentBlock: React.FC<Props> = ({color, title, content, onClick}) => {
             transform hover:scale-105 transition-all duration-200 ease-in-out hover:shadow-2xl cursor-pointer`}
             onClick={onClick}
         >
-            <h2 className={`font-bold italic text-xl mb-2 text-${color} group-hover:text-white`}>{title}</h2>
-            <hr className={`border-${color} group-hover:border-white`}/>
-            <p className={`font-light text-sm font-sans-content my-2 text-black`}>
+            <h2 className={`font-bold italic text-3xl text-${color} group-hover:text-white`}>{title}</h2>
+            <hr className={`border-${color} group-hover:border-white my-4`}/>
+            <p className={`font-light text-lg font-sans-content text-black`}>
                 {content || "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean\n" +
                 "                massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam\n" +
                 "                felis, ultricies nec, light 30"}
