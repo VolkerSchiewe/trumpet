17204b811a1f0cd3bd72fa91fc700b28f77a465d
VolkerSchiewe <volker.s1994@gmail.com>
2020-06-20 18:02:51 +0200

    Integrate next server coverage, upload coverage report

--- test/integration/landing.ts
+++ test/integration/landing.ts
@@ -1,8 +1,7 @@
 describe('The landing page', () => {
-    // it('successfully loads', () => {
-    // });
     it('should navigate to registration', function () {
         cy.visit('/')
         cy.get('#Anmeldung').click()
+        cy.url().should('include', '/registration')
     });
 });
