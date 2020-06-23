aec3fb32a90c28b25fb71e7d139a081872ac1526
VolkerSchiewe <volker.s1994@gmail.com>
2020-06-23 11:47:23 +0200

    Fix tailwind purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     purge: {
         content: [
-            './components/**/*.tsx',
+            './src/**/*.tsx',
             './pages/**/*.tsx',
         ],
         options: {
