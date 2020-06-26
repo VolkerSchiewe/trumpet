a2ada6b27360435ef70eb1ce5762fb6c7984dc8c
VolkerSchiewe <volker.s1994@gmail.com>
2020-06-26 20:52:40 +0200

    Improve email verification coverage

--- cypress/integration/email-verification.test.ts
+++ cypress/integration/email-verification.test.ts
@@ -4,13 +4,25 @@ describe("Email verification", () => {
     afterEach(() => {
         cy.request("/api/__delete-test-data__")
     })
-    it('should verify registration', function () {
+    it('should verify registration and fail on second try', function () {
         cy.request("POST", "/api/registration", userData)
             .then(({body}) => {
                 cy.visit(`/verify-mail?token=${body}`)
                 cy.findByText(/Deine E-Mail Addresse wurde bestätigt./).should("exist")
-                cy.wait(5000)
-                cy.location('pathname').should('eq', '/')
+
+                cy.visit(`/verify-mail?token=${body}`)
+                cy.findByText(/bereits bestätigt/).should("exist")
             })
     })
+
+    it('should fail for invalid link', function () {
+        cy.visit(`/verify-mail?token=InvalidToken`)
+        cy.findByText("Falscher Link")
+    });
+
+    it('should fail if link is expired', function () {
+        // Using an old database entry
+        cy.visit(`/verify-mail?token=T1ArGhThh6lBmz8OsrmE`)
+        cy.findByText(/abgelaufen/)
+    });
 })
