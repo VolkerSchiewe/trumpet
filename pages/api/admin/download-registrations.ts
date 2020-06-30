1a9ab5c9db0ee58fd495fdb6640063af8897ec57
VolkerSchiewe <volker.s1994@gmail.com>
2020-06-30 15:24:25 +0200

    Remove usage of fs

--- pages/api/admin/download-registrations.ts
+++ pages/api/admin/download-registrations.ts
@@ -1,10 +1,8 @@
-import * as fs from "fs";
 import {NextApiRequest, NextApiResponse} from 'next'
-import {WritingOptions} from "xlsx";
+import * as XLSX from "xlsx";
 import {getAllRegistrations} from "../../../src/api/getAllRegistrations";
 import {localizeDataExport} from "../../../src/api/localizeDataExport";
 import {restrictRoute} from "../../../src/api/restrictRoute";
-import * as XLSX from 'xlsx';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (await restrictRoute(req, res, "GET", true))
@@ -17,14 +15,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     const d = new Date()
     const filename = `Anmeldungen-${d.getDate()}_${d.getMonth() + 1}_${d.getFullYear()}.xlsx`
-    const wb_opts: WritingOptions = {bookType: 'xlsx', type: 'binary'};
-    XLSX.writeFile(wb, filename, wb_opts);
-    const stream = fs.createReadStream(filename);
+    const wbBuffer = XLSX.writeFile(wb, filename, {bookType: 'xlsx', type: 'buffer'});
     res.writeHead(200, {
         'Content-Type': 'application/xlsx',
         'Content-disposition': `attachment; filename=${filename}`
     });
-    stream.pipe(res);
-    fs.unlink(filename, () => {
-    })
+    res.send(wbBuffer)
+
 }
