ea42b925eb1e246fefdacc38e732343acf4bfecd
VolkerSchiewe <volker.s1994@gmail.com>
2020-07-06 21:28:54 +0200

    Additional image for landing page slider

--- src/components/landing/LandingCarousel.tsx
+++ src/components/landing/LandingCarousel.tsx
@@ -14,12 +14,19 @@ const LandingCarousel: React.FC = () => (
             </picture>
             <img className="w-1/3 px-1" src={"images/logo.svg"} alt={"logo"}/>
         </div>
-        <div className='bg-white flex flex-row '>
+        <div className='bg-white flex flex-row'>
             <img className="w-1/3 p-1" src={"images/logo.svg"} alt={"logo"}/>
             <picture className="w-2/3 p-2">
                 <source srcSet={"images/oberbaumbruecke-slogan.webp"} type="image/webp"/>
                 <img src={"images/oberbaumbruecke-slogan.jpg"} alt={"Oberbaumbrücke"}/>
             </picture>
+        </div>
+        <div className='bg-white flex flex-row'>
+            <picture className="w-2/3 p-2">
+                <source srcSet={"images/herrnhuter-weg-slogan.webp"} type="image/webp"/>
+                <img src={"images/herrnhuter-weg-slogan.jpg"} alt={"Herrnhuter Weg Schild mit Logo des Bläsertags"}/>
+            </picture>
+            <img className="w-1/3 p-1" src={"images/logo.svg"} alt={"logo"}/>
         </div>
     </Carousel>
 )
