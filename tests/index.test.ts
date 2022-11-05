659ce72a9e92ab3a9357b4e2402cc4b4f625445e
Volker <volker@schiewe.dev>
2022-11-05 18:37:49 +0100

    Format

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,10 +2,18 @@ import { expect, test } from '@playwright/test';
 
 test('index page renders correctly', async ({ page }) => {
 	await page.goto('/');
-	
-	await expect(page.getByRole('heading', { name: '39. Brüderisches Bläsertreffen' })).toBeVisible()
-	await expect(page.getByRole('img', { name: 'Moravian Brass Festival Berlin 2023' })).toBeVisible()
-	
-	await expect(page.getByRole("link", {name: /Anmeldung/})).toHaveAttribute('href', "/registration")
-	await expect(page.getByRole("link", {name: "Berlin unsere Gemeinde"})).toHaveAttribute('href', "https://www.herrnhuter-in-berlin.de/")
+
+	await expect(page.getByRole('heading', { name: '39. Brüderisches Bläsertreffen' })).toBeVisible();
+	await expect(
+		page.getByRole('img', { name: 'Moravian Brass Festival Berlin 2023' })
+	).toBeVisible();
+
+	await expect(page.getByRole('link', { name: /Anmeldung/ })).toHaveAttribute(
+		'href',
+		'/registration'
+	);
+	await expect(page.getByRole('link', { name: 'Berlin unsere Gemeinde' })).toHaveAttribute(
+		'href',
+		'https://www.herrnhuter-in-berlin.de/'
+	);
 });
