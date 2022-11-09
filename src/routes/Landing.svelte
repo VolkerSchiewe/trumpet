f5976909756ec18d917b3d7d440aa510ca01c812
Volker <volker@schiewe.dev>
2022-11-09 17:42:00 +0100

    Minor improvements

--- src/routes/Landing.svelte
+++ src/routes/Landing.svelte
@@ -4,10 +4,10 @@
 
 <div class="w-full md:relative">
 	<div
-		class="z-10 flex w-full flex-col items-center gap-8 bg-theme-purple/80 p-4 py-8 text-white md:absolute md:top-0 md:right-0 md:bottom-0 md:w-96 md:items-start"
+		class="z-10 flex w-full flex-col items-center gap-8 bg-theme-purple/80 p-4 py-8 text-white md:absolute md:top-0 md:right-0 md:bottom-0 md:w-96 md:items-start md:p-8"
 	>
 		<div class="flex flex-col items-center md:items-start">
-			<h1 class="mt-6 text-lg md:mt-3">39. Brüderisches Bläsertreffen</h1>
+			<h1 class="mt-6 text-lg md:mt-3 font-light">39. Brüderisches Bläsertreffen</h1>
 			<hr class="mt-2 w-20 border-2 border-theme-yellow" />
 		</div>
 		<img class="max-w-xl md:hidden" src="logo.svg" alt="" />
