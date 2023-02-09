2acbb53e92e51fea67183f53b3888c9d8a0f1679
Volker <volker@schiewe.dev>
2023-02-09 17:50:20 +0100

    Update program

--- src/routes/program/+page.svelte
+++ src/routes/program/+page.svelte
@@ -17,37 +17,44 @@
 		</p>
 		<Header color={'green'} title="Freitag" subTitle="26.05.2023" />
 		<table class="block">
-			<tr>
-				<td class="text-end">ab 16:00 Uhr •</td> <td class="p-1"> Anreise</td>
-			</tr>
-			<tr>
-				<td class="text-end">18:00 Uhr •</td> <td class="p-1"> Abendbrot</td>
-			</tr>
-			<tr>
-				<td class="text-end">20:30 Uhr •</td> <td class="p-1"> Eröffnung</td>
-			</tr>
+			<li>Anreise</li>
+			<li>Abendbrot</li>
+			<li>Eröffnung/Abendsegen</li>
+			<li>anschl. gemütliches Beisammensein</li>
 		</table>
 		<Header color={'green'} title="Samstag" subTitle="27.05.2023" />
 		<ul class="mb-8 list-disc">
-			<li>Morgensegen Gesamtprobe</li>
-			<li>Workshops</li>
+			<li>Morgensegen</li>
+			<li>1. Gesamtprobe</li>
+			<li>Kaffee/Teepause</li>
+			<li>2. Gesamtprobe</li>
 			<li>Mittagessen</li>
-			<li>Führungen Böhm. Dorf, Comenius Garten</li>
-			<li>Abendmahlsgottesdienst</li>
+			<li>3. Gesamtprobe</li>
+			<li>Kaffee/Teepause</li>
+			<li>Workshops</li>
+			<li>Abendbrot</li>
+			<li>Singstunde</li>
+			<li>Vorstellung der Ergebnisse der Workshops</li>
+			<li>anschl. gemütliches Beisammensein</li>
 		</ul>
 	</slot>
 	<slot slot="right">
 		<Header title="Sonntag" subTitle="28.05.2023" />
 		<ul class="list-disc">
-			<li>Gottesdienst in verschiedenen Gemeinden</li>
-			<li>Hauptprobe</li>
+			<li>Gottesdienste in verschiedenen Gemeinden</li>
+			<li>Mittagessen</li>
+			<li>4. Gesamtprobe</li>
 			<li>Konzert</li>
+			<li>Gruppenfoto</li>
+			<li>Abendbrot</li>
 			<li>Bunter Abend</li>
+			<li>anschl. gemütliches Beisammensein</li>
 		</ul>
 		<Header title="Montag" subTitle="29.05.2023" />
 		<ul class="mb-8 list-disc">
-			<li>Gottesdienst</li>
-			<li>Ausflug (Schifffahrt ca. 3 Std.)</li>
+			<li>Aufblasen der Jungbläser</li>
+			<li>Gemeinsame Abendmahlsfeier mit Predigt</li>
+			<li>Ausflug (Schifffahrt ca. 3 Std.) inkl. Mittagsimbiss</li>
 			<li>Verabschiedung vorher für die, die eher abfahren oder im Anschluss</li>
 		</ul>
 	</slot>
