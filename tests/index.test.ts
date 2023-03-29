d3f1b4a61e03c210d23e5510f3b28d4a32a54e3b
Volker <volker@schiewe.dev>
2023-03-29 08:36:48 +0200

    Adjust integration tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -8,10 +8,7 @@ test('index page renders correctly', async ({ page }) => {
 		page.getByRole('img', { name: 'Moravian Brass Festival Berlin 2023' })
 	).toBeVisible();
 
-	await expect(page.getByRole('link', { name: /Anmeldung/ })).toHaveAttribute(
-		'href',
-		'/registration'
-	);
+	await expect(page.getByRole('link', { name: /Details/ })).toHaveAttribute('href', '/information');
 	await expect(page.getByRole('link', { name: 'Berlin unsere Gemeinde' })).toHaveAttribute(
 		'href',
 		'https://www.herrnhuter-in-berlin.de/'
