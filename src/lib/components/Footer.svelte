af500f8486df843a9003abaee4551316d352a348
Volker <volker@schiewe.dev>
2023-05-27 08:55:44 +0200

    fix footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -9,11 +9,11 @@
 		<img class="aspect-[16/6] w-full object-cover object-bottom" src={image.src} alt={image.alt} />
 	{/if}
 	<div class="flex flex-col items-center gap-5 bg-theme-purple py-10 text-white">
-		<div class="flex gap-5 font-bold">
-			<a href="mailto:info@blaesertag2023.de"> Kontakt </a>
-			<a href="https://www.herrnhuter-in-berlin.de/"> Brüdergemeine Berlin </a>
-			<a href="https://www.herrnhuter-in-berlin.de/impressum"> Impressum </a>
-			<a href="/data-protection"> Datenschutz </a>
+		<div class="mx-5 grid grid-cols-2 justify-center gap-5 font-bold md:grid-cols-4">
+			<a class="text-center" href="mailto:info@blaesertag2023.de"> Kontakt </a>
+			<a class="text-center" href="https://www.herrnhuter-in-berlin.de/"> Brüdergemeine Berlin </a>
+			<a class="text-center" href="https://www.herrnhuter-in-berlin.de/impressum"> Impressum </a>
+			<a class="text-center" href="/data-protection"> Datenschutz </a>
 		</div>
 		<span class="text-sm">© 2022 Evangelische Brüdergemeine Berlin </span>
 	</div>
