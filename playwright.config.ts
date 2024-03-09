eeb76eb81b7a524d72114f30b59d4388cf8fc995
Volker <volker@schiewe.dev>
2024-03-09 18:11:30 +0100

    remove base url in ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,15 +2,15 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	testDir: 'tests',
-	webServer: process.env.CI
-		? undefined
-		: {
+	webServer: !process.env.CI
+		? {
 				command: 'yarn dev --host',
 				port: 5173
-			},
+			}
+		: undefined,
 	use: {
 		browserName: 'chromium',
-		baseURL: 'http://localhost:5173/',
+		baseURL: !process.env.CI ? 'http://localhost:5173/' : undefined,
 		screenshot: { mode: 'only-on-failure', fullPage: true }
 	}
 };
